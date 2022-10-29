d8a5da73d1248412d7d28b986d99aee030dce7f1
somnath <somnathnath8482@gmail.com>
2022-10-29 16:56:03 +0530

    smn

--- NetworkRequest/src/main/java/com/artix/networkrequest/Main.java
+++ NetworkRequest/src/main/java/com/artix/networkrequest/Main.java
@@ -8,9 +8,21 @@ import androidx.annotation.Nullable;
 import com.artix.networkrequest.Interface.OnError;
 import com.artix.networkrequest.Interface.OnSuccess;
 
+
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import org.apache.http.impl.client.HttpClients;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
@@ -35,17 +47,17 @@ public class Main {
         this.onSuccess = onSuccess;
     }
 
-    public void GetRequest(String url, @Nullable String authHeader){
-        authHeader = authHeader==null? "":authHeader;
+    public void CALLGetRequest(String url, @Nullable String authHeader) {
+        authHeader = authHeader == null ? "" : authHeader;
 
         try {
             String finalAuthHeader = authHeader;
             new Thread(
-                   () -> {
+                    () -> {
 
-                       try {
-                           URL ur_l = new URL(url);
-                          URLConnection myURLConnection = ur_l.openConnection();
+                        try {
+                            URL ur_l = new URL(url);
+                            URLConnection myURLConnection = ur_l.openConnection();
 
                           /* HttpsURLConnection myURLConnection = (HttpsURLConnection)ur_l.openConnection();
 
@@ -65,36 +77,34 @@ public class Main {
 
                             myURLConnection.setRequestProperty("Authorization",
                                     finalAuthHeader);
-                           InputStream is = myURLConnection.getInputStream();
-                           Log.d(TAG, "GetRequest: "+is.toString());
+                            InputStream is = myURLConnection.getInputStream();
+                            Log.d(TAG, "GetRequest: " + is.toString());
 
 
-                           StringWriter writer = new StringWriter();
-                           IOUtils.copy(is, writer, "UTF-8");
-                           String theString = writer.toString();
-                           activity.runOnUiThread(() -> {
-                               onSuccess.OnSucces(url,"200", theString);
-                           });
-                       } catch (IOException e) {
+                            StringWriter writer = new StringWriter();
+                            IOUtils.copy(is, writer, "UTF-8");
+                            String theString = writer.toString();
+                            activity.runOnUiThread(() -> {
+                                onSuccess.OnSucces(url, "200", theString);
+                            });
+                        } catch (IOException e) {
 
-                          activity.runOnUiThread(() -> {
-                              onError.OnEror(url,1+"", e.getMessage());
-                          });
-                       }
+                            activity.runOnUiThread(() -> {
+                                onError.OnEror(url, 1 + "", e.getMessage());
+                            });
+                        }
 
-                   }
-           ).start();
+                    }
+            ).start();
         } catch (Exception e) {
-            onError.OnEror(url,1+"", e.getMessage());
+            onError.OnEror(url, 1 + "", e.getMessage());
         }
 
     }
 
 
-
-
-    public void CallPostRequestJSon(String url, @Nullable String authHeader, JSONObject object){
-        authHeader = authHeader==null? "":authHeader;
+    public void CallPostRequestJSon(String url, @Nullable String authHeader, JSONObject object) {
+        authHeader = authHeader == null ? "" : authHeader;
 
         try {
             String finalAuthHeader = authHeader;
@@ -102,21 +112,21 @@ public class Main {
                     () -> {
 
                         try {
-                            byte[] postData       = object.toString().getBytes("UTF-8");
+                            byte[] postData = object.toString().getBytes("UTF-8");
                             int postDataLength = postData.length;
 
 
                             URL ur_l = new URL(url);
-                            HttpsURLConnection myURLConnection = (HttpsURLConnection)ur_l.openConnection();
-                           myURLConnection.setRequestMethod("POST");
-                        // myURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                      //myURLConnection.setRequestProperty("Content-Type", "application/json");
-                          myURLConnection.setRequestProperty("Content-Length", "" + postDataLength);
-                           //myURLConnection.setRequestProperty("Content-Language", "en-US");
-                          myURLConnection.setUseCaches(false);
-                          myURLConnection.setDoInput(true);
-                          myURLConnection.setDoOutput(true);
-                           myURLConnection.setRequestProperty( "Accept", "*/*" );
+                            HttpsURLConnection myURLConnection = (HttpsURLConnection) ur_l.openConnection();
+                            myURLConnection.setRequestMethod("POST");
+                            // myURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                            //myURLConnection.setRequestProperty("Content-Type", "application/json");
+                            myURLConnection.setRequestProperty("Content-Length", "" + postDataLength);
+                            //myURLConnection.setRequestProperty("Content-Language", "en-US");
+                            myURLConnection.setUseCaches(false);
+                            myURLConnection.setDoInput(true);
+                            myURLConnection.setDoOutput(true);
+                            myURLConnection.setRequestProperty("Accept", "*/*");
                             myURLConnection.setRequestProperty("Authorization", finalAuthHeader);
                             myURLConnection.getOutputStream().write(postData);
 
@@ -128,26 +138,26 @@ public class Main {
                             IOUtils.copy(is, writer, "UTF-8");
                             String theString = writer.toString();
                             activity.runOnUiThread(() -> {
-                                onSuccess.OnSucces(url,"200", theString);
+                                onSuccess.OnSucces(url, "200", theString);
                             });
                         } catch (IOException e) {
 
                             activity.runOnUiThread(() -> {
-                                onError.OnEror(url,1+"", e.getMessage());
+                                onError.OnEror(url, 1 + "", e.getMessage());
                             });
                         }
 
                     }
             ).start();
         } catch (Exception e) {
-            onError.OnEror(url,1+"", e.getMessage());
+            onError.OnEror(url, 1 + "", e.getMessage());
         }
 
 
-
     }
-    public void CallPostRequestFormdata(String url, @Nullable String authHeader, HashMap<String, Object> map){
-        authHeader = authHeader==null? "":authHeader;
+
+    public void CallPostRequestFormdata(String url, @Nullable String authHeader, HashMap<String, Object> map) {
+        authHeader = authHeader == null ? "" : authHeader;
 
         try {
             String finalAuthHeader = authHeader;
@@ -157,7 +167,7 @@ public class Main {
                         try {
 
                             StringBuilder postData = new StringBuilder();
-                            for (Map.Entry<String,Object> param : map.entrySet()) {
+                            for (Map.Entry<String, Object> param : map.entrySet()) {
                                 if (postData.length() != 0) postData.append('&');
                                 postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
                                 postData.append('=');
@@ -170,16 +180,16 @@ public class Main {
 
 
                             URL ur_l = new URL(url);
-                            HttpsURLConnection myURLConnection = (HttpsURLConnection)ur_l.openConnection();
-                           myURLConnection.setRequestMethod("POST");
-                       myURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                      //myURLConnection.setRequestProperty("Content-Type", "application/json");
-                          myURLConnection.setRequestProperty("Content-Length", "" + postDataLength);
-                           //myURLConnection.setRequestProperty("Content-Language", "en-US");
-                          myURLConnection.setUseCaches(false);
-                          myURLConnection.setDoInput(true);
-                          myURLConnection.setDoOutput(true);
-                           myURLConnection.setRequestProperty( "Accept", "*/*" );
+                            HttpsURLConnection myURLConnection = (HttpsURLConnection) ur_l.openConnection();
+                            myURLConnection.setRequestMethod("POST");
+                            myURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                            //myURLConnection.setRequestProperty("Content-Type", "application/json");
+                            myURLConnection.setRequestProperty("Content-Length", "" + postDataLength);
+                            //myURLConnection.setRequestProperty("Content-Language", "en-US");
+                            myURLConnection.setUseCaches(false);
+                            myURLConnection.setDoInput(true);
+                            myURLConnection.setDoOutput(true);
+                            myURLConnection.setRequestProperty("Accept", "*/*");
                             myURLConnection.setRequestProperty("Authorization", finalAuthHeader);
                             myURLConnection.getOutputStream().write(postDataBYTE);
 
@@ -191,22 +201,76 @@ public class Main {
                             IOUtils.copy(is, writer, "UTF-8");
                             String theString = writer.toString();
                             activity.runOnUiThread(() -> {
-                                onSuccess.OnSucces(url,"200", theString);
+                                onSuccess.OnSucces(url, "200", theString);
                             });
                         } catch (IOException e) {
 
                             activity.runOnUiThread(() -> {
-                                onError.OnEror(url,1+"", e.getMessage());
+                                onError.OnEror(url, 1 + "", e.getMessage());
                             });
                         }
 
                     }
             ).start();
         } catch (Exception e) {
-            onError.OnEror(url,1+"", e.getMessage());
+            onError.OnEror(url, 1 + "", e.getMessage());
         }
 
 
+    }
 
+    public void CAllMultipartRequest(String url, @Nullable String authHeader, HashMap<String, Object> map,File f) {
+        authHeader = authHeader == null ? "" : authHeader;
+
+        try {
+            String finalAuthHeader = authHeader;
+            new Thread(
+                    () -> {
+
+                        try {
+                            CloseableHttpClient httpClient = HttpClients.createDefault();
+                            URL ur_l = new URL(url);
+                            HttpPost uploadFile = new HttpPost(url);
+                            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                            builder.addTextBody("field1", "yes", ContentType.TEXT_PLAIN);
+                           // myURLConnection.setRequestProperty("Authorization", finalAuthHeader);
+
+
+                            builder.addBinaryBody(
+                                    "file",
+                                    new FileInputStream(f),
+                                    ContentType.APPLICATION_OCTET_STREAM,
+                                    f.getName()
+                            );
+
+                            HttpEntity multipart = builder.build();
+                            uploadFile.setEntity(multipart);
+                            CloseableHttpResponse response = httpClient.execute(uploadFile);
+                            HttpEntity responseEntity = response.getEntity();
+
+
+
+
+                            InputStream is = responseEntity.getContent();
+
+
+                            StringWriter writer = new StringWriter();
+                            IOUtils.copy(is, writer, "UTF-8");
+                            String theString = writer.toString();
+                            activity.runOnUiThread(() -> {
+                                onSuccess.OnSucces(url, "200", theString);
+                            });
+                        } catch (Exception e) {
+
+                            activity.runOnUiThread(() -> {
+                                onError.OnEror(url, 1 + "", e.getMessage());
+                            });
+                        }
+
+                    }
+            ).start();
+        } catch (Exception e) {
+            onError.OnEror(url, 1 + "", e.getMessage());
+        }
     }
 }
--- app/src/main/java/com/artix/networkrequest/MainActivity.java
+++ app/src/main/java/com/artix/networkrequest/MainActivity.java
@@ -1,24 +1,32 @@
 package com.artix.networkrequest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.artix.networkrequest.Helper.Conntants;
 import com.artix.networkrequest.Helper.MethodClass;
 import com.artix.networkrequest.Interface.OnError;
 import com.artix.networkrequest.Interface.OnSuccess;
 import com.artix.networkrequest.databinding.ActivityMainBinding;
+import com.hbisoft.pickit.PickiT;
+import com.hbisoft.pickit.PickiTCallbacks;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity implements OnError, OnSuccess {
+public class MainActivity extends AppCompatActivity implements OnError, OnSuccess, PickiTCallbacks {
     ActivityMainBinding binding;
     private Main main;
+    PickiT pickiT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,53 +35,52 @@ public class MainActivity extends AppCompatActivity implements OnError, OnSucces
         View view = binding.getRoot();
         setContentView(view);
         //setContentView(R.layout.activity_main);
+        pickiT = new PickiT(this, this, this);
+        main = new Main(this, this, this);
 
 
-
-         main = new Main(this,this, this);
-
-
-       Onclick();
+        Onclick();
     }
 
     private void Onclick() {
-        binding.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                @SuppressLint("HardwareIds") String android_id = Settings.Secure.getString(getContentResolver(),
-                        Settings.Secure.ANDROID_ID);
-                String url = "get-cart/" + android_id;
-                main.GetRequest("https://www.alaaddin.in/preview/api/"+url, Conntants.AUTHHEADER);
-            }
+        binding.button.setOnClickListener(view -> {
+            @SuppressLint("HardwareIds") String android_id = Settings.Secure.getString(getContentResolver(),
+                    Settings.Secure.ANDROID_ID);
+            String url = "get-cart/" + android_id;
+            main.CALLGetRequest("https://www.alaaddin.in/preview/api/" + url, Conntants.AUTHHEADER);
         });
 
-        binding.button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                HashMap<String, String> map = new HashMap<>();
-                map.put("email","buyer@gmail.com");
-               main.CallPostRequestJSon("https://www.alaaddin.in/preview/api/resend-code", Conntants.AUTHHEADER, MethodClass.Json_rpc_format(map));
-              // main.CallPostRequestJSon("https://artixdevl.000webhostapp.com/api/test.php",Conntants.AUTHHEADER,map);
-            }
+        binding.button2.setOnClickListener(view -> {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("email", "buyer@gmail.com");
+            main.CallPostRequestJSon("https://www.alaaddin.in/preview/api/resend-code", Conntants.AUTHHEADER, MethodClass.Json_rpc_format(map));
+            // main.CallPostRequestJSon("https://artixdevl.000webhostapp.com/api/test.php",Conntants.AUTHHEADER,map);
         });
 
-binding.button3.setOnClickListener(view -> {
-    HashMap<String, Object> map = new HashMap<>();
-    map.put("password","123e456");
-    map.put("new_password","123123123");
-    map.put("password_confirmation", "123123123");
+        binding.button3.setOnClickListener(view -> {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("password", "123e456");
+            map.put("new_password", "123123123");
+            map.put("password_confirmation", "123123123");
 
-    main.CallPostRequestFormdata("https://www.alaaddin.in/preview/api/user-change-password",Conntants.AUTHHEADER,map);
-    //main.CallPostRequestFormdata("https://artixdevl.000webhostapp.com/api/test.php",Conntants.AUTHHEADER,map);
+            main.CallPostRequestFormdata("https://www.alaaddin.in/preview/api/user-change-password", Conntants.AUTHHEADER, map);
+            //main.CallPostRequestFormdata("https://artixdevl.000webhostapp.com/api/test.php",Conntants.AUTHHEADER,map);
 
-});
+        });
+
+        binding.button4.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 100);
+        });
     }
 
 
     @Override
     public void OnEror(String url, String code, String message) {
 
-        Toast.makeText(this, "Error in url"+url+"\n "+message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Error in url" + url + "\n " + message, Toast.LENGTH_SHORT).show();
 
     }
 
@@ -84,4 +91,53 @@ binding.button3.setOnClickListener(view -> {
 
 
     }
+
+    @Override
+    public void PickiTonUriReturned() {
+
+    }
+
+    @Override
+    public void PickiTonStartListener() {
+
+    }
+
+    @Override
+    public void PickiTonProgressUpdate(int progress) {
+
+    }
+
+    @Override
+    public void PickiTonCompleteListener(String path, boolean wasDriveFile, boolean wasUnknownProvider, boolean wasSuccessful, String Reason) {
+        if (wasSuccessful) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("Name", "Somnath");
+            main.CAllMultipartRequest("https://artixdevl.000webhostapp.com/api/test.php",
+                    Conntants.AUTHHEADER,
+                    map
+                    ,
+                    new File(path));
+
+
+        }
+    }
+
+
+    @Override
+    public void PickiTonMultipleCompleteListener(ArrayList<String> paths, boolean wasSuccessful, String Reason) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            pickiT.getPath(data.getData(), Build.VERSION.SDK_INT);
+
+
+        }
+    }
+
+
 }
